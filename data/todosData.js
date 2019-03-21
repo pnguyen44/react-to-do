2a1e517621e6e2336bcd26bdd48a1f7db0ce2d3f
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-03-21 08:35:44 -0400

    Updates

---
+++ data/todosData.js
@@ -0,0 +1,24 @@
+const todosData = [
+    {
+        id: 1,
+        text: "Do code challenges on HankerRank",
+        completed: true
+    },
+    {
+        id: 2,
+        text: "Grocery shopping",
+        completed: false
+    },
+    {
+        id: 4,
+        text: "Finish angular tutorials",
+        completed: true
+    },
+    {
+        id: 5,
+        text: "Watch Umbrella Academy",
+        completed: false
+    }
+]
+
+export default todosData
