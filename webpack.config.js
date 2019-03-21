265cae8fceee7f7917cf9a4fdcf2a5d6d0293573
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-03-21 02:59:23 -0400

    Update webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,29 @@
 const path = require('path')
 
 module.exports = {
+  mode: 'none',
   entry: {
     app: [
-      './app/index'
+      './app/index.js'
     ],
   },
   output: {
+    // path: __dirname + "/build/",
     path: path.resolve(__dirname, 'build'),
-    filename: '[name].pack.js'
+    filename: 'index.bundle.js'
   },
   module: {
-    rules: [
-      {
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              "babel-preset-env",
-              "babel-preset-react"
-            ]
-          }
-        },
-        exclude: /node_modules/,
-        test: /\.js$/
+    rules: [{
+      test: /\.js?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: [
+          "babel-preset-env",
+          "babel-preset-react",
+          'env'
+        ]
       }
-    ]
+    }]
   }
 }
