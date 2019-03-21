88d4dc4da324eff4a89b71dc66be23a2e3025ce8
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-03-21 03:32:16 -0400

    Add on change event handler

--- app/App.js
+++ app/App.js
@@ -3,8 +3,29 @@ import todosData from './data/todosData'
 import TodoItem from './components/TodoItem'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: todosData
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+  handleChange(id) {
+    this.setState(prevState => {
+      const updatedTodoItems = prevState.todos.map(item => {
+        if(item.id === id) {
+          item.completed = !item.completed
+        }
+        return item
+      })
+      return {
+        todos: updatedTodoItems
+      }
+    })
+  }
   render() {
-    const todoItemComponents = todosData.map(item => <TodoItem key={item.id} item={item}/>)
+    const todoItemComponents = this.state.todos.map(item => <TodoItem key={item.id} item={item}
+      handleChange={this.handleChange}/>)
     return (
       <div className="todo-list">
         {todoItemComponents}
--- app/components/TodoItem.js
+++ app/components/TodoItem.js
@@ -7,6 +7,7 @@ class todoItem extends React.Component {
         <input
           type='checkbox'
           checked={this.props.item.completed}
+          onChange={() => this.props.handleChange(this.props.item.id)}
         />
         <p>{this.props.item.text}</p>
       </div>
