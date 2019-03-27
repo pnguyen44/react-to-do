10a7dec5cdf12a0f667321e7edb983cca1b9547b
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-03-27 15:34:25 -0400

    Add data for todos

---
+++ src/todosData.js
@@ -0,0 +1,24 @@
+const todosData = [
+    {
+        id: 1,
+        name: "Do code challenges on HankerRank",
+        completed: true
+    },
+    {
+        id: 2,
+        name: "Grocery shopping",
+        completed: false
+    },
+    {
+        id: 4,
+        name: "Finish angular tutorials",
+        completed: true
+    },
+    {
+        id: 5,
+        name: "Watch Umbrella Academy",
+        completed: false
+    }
+]
+
+export default todosData
