c5143f1e606fdcb7c062e090c195f30f365b236f
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-03-27 16:15:46 -0400

    Update todo list style

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 
 class TodoItem extends Component {
+
   getStyles = () => {
     return {
       background: '#f4f4f4',
