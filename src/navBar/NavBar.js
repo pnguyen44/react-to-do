45a5faacb5b66500f91eab99fb1cab3426b01781
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-03-29 18:56:12 -0400

    Add to-do icon in navbar

--- src/navBar/NavBar.js
+++ src/navBar/NavBar.js
@@ -32,6 +32,7 @@ const NavBar = () => {
     <div style={styles.root}>
       <AppBar position="static">
         <Toolbar>
+        <img src={require('../to-do-icon.png')} style={{width:'58px', marginLeft:'1vw', paddingRight:'14px'}} />
           <Typography variant="h6" color="inherit" style={styles.grow}>
             On Track
           </Typography>
