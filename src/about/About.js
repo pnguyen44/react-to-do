5502181bb777d00808f70652634e5e164bdede7c
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-03-29 16:22:50 -0400

    Update about page content style

--- src/about/About.js
+++ src/about/About.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
+const styles = {
+  textAlign: 'center'
+}
+
 function About() {
   return (
     <React.Fragment>
-      <h2>A to-do app built using react.</h2>
+      <h2 style={styles}>A to-do app built using react.</h2>
     </React.Fragment>
   )
 }
