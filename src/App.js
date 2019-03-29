d482b2513a0129d40087c067cc9bce4ea7f79e2b
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-03-29 09:13:25 -0400

    Update removeItem and newTodoItem functions

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css';
-import todosData from './todosData'
+// import todosData from './todosData'
 import Todos from './todo/Todos'
 import Header from './header/Header'
 import NewTodoItem from './todo/components/NewTodoItem'
 import About from './about/About'
-
+import apiUrl from './apiConfig'
+import axios from 'axios'
 
 
 class App extends Component {
   state = {
-    todos: todosData
+    todos: []
   }
+
+componentDidMount() {
+  // fetch('https://jsonplaceholder.typicode.com/todos?_limit=10')
+  fetch(apiUrl + '/items')
+    .then(res => {
+      return res.json()
+    })
+    .then(data => {
+      this.setState({todos: data})
+    })
+}
 
 toogleComplete = (id) => {
   this.setState({ todos: this.state.todos.map(item => {
@@ -24,18 +36,39 @@ toogleComplete = (id) => {
 }
 
 removeItem = (id) => {
-  console.log(id)
-  this.setState({todos: [...this.state.todos].filter(item => item.id !== id)})
+  axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .then(res => this.setState({ todos: [...this.state.todos.filter(todo => todo._id !== id)] }));
 }
 
 newTodoItem = (name) => {
-  console.log(name)
-  const newTodoItem = {
-    id: 211,
+  // const todoItem = {
+  //   // id: 10099,
+  //   name: name,
+  //   completed: false,
+  // }
+
+  // console.log(name)
+  // const url = 'https://jsonplaceholder.typicode.com/todos'
+  // return axios(url,todoItem)
+  //     .then(res => this.setState({todos: [...this.state.todos,res.data]}))
+  // const init = {
+  //   method: 'POST',
+  //   // headers: {
+  //   //   'Content-Type': 'application/json',
+  //   // },
+  //   body: JSON.stringify(todoItem)
+  // }
+  // fetch(apiUrl + '/items', init)
+  //   .then(res => {
+  //     console.log('...', res)
+  //     this.setState({todos: [...this.state.todos,res.data]})
+  //   })
+
+  axios.post(apiUrl + '/items', {
     name,
     completed: false
-  }
-  this.setState({todos: [...this.state.todos,newTodoItem]})
+  })
+  .then(res => this.setState({ todos: [...this.state.todos, res.data] }));
 }
 
   render() {
