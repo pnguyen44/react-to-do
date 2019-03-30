1146f693e40d2ea2f8b3a1d23f9430a62aaf468e
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-03-30 07:12:49 -0400

    Updates

--- precache-manifest.771d35dc8f01f13c22fec4f71edfcbc2.js
+++ precache-manifest.771d35dc8f01f13c22fec4f71edfcbc2.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "5f5cc119f696931eeaf8",
-    "url": "/on-track/static/css/main.0ac514a7.chunk.css"
+    "revision": "9d6d80e8e69f5af3dd6b",
+    "url": "/on-track/static/css/main.e36d538e.chunk.css"
   },
   {
-    "revision": "5f5cc119f696931eeaf8",
-    "url": "/on-track/static/js/main.1895f65e.chunk.js"
+    "revision": "9d6d80e8e69f5af3dd6b",
+    "url": "/on-track/static/js/main.a7675d39.chunk.js"
   },
   {
     "revision": "dc7764a5a00f8dbb8787",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/on-track/static/media/to-do-icon.a92de81b.png"
   },
   {
-    "revision": "be6d49c31445117a70890e3180502c25",
+    "revision": "e96deb4198cdf2ebecac1561f3d7f81d",
     "url": "/on-track/index.html"
   }
 ];
