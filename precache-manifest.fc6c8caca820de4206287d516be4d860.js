1a86eff8ca62a356187263fd5b0a79f279a5fb5f
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-04-10 12:13:48 -0400

    Updates

--- precache-manifest.fc6c8caca820de4206287d516be4d860.js
+++ precache-manifest.fc6c8caca820de4206287d516be4d860.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
-    "revision": "a45bcc604e5a619ec598",
+    "revision": "7ca15a31c3ab873ce2d6",
     "url": "/on-track/static/css/main.ff16afa7.chunk.css"
   },
   {
-    "revision": "a45bcc604e5a619ec598",
-    "url": "/on-track/static/js/main.3919f7ca.chunk.js"
+    "revision": "7ca15a31c3ab873ce2d6",
+    "url": "/on-track/static/js/main.cafa61fb.chunk.js"
   },
   {
     "revision": "dc7764a5a00f8dbb8787",
     "url": "/on-track/static/js/runtime~main.8ea0a5a9.js"
   },
   {
-    "revision": "26d574b5469fe7f46f15",
-    "url": "/on-track/static/js/2.65f4d22d.chunk.js"
+    "revision": "e3ba4e5e93c598e3956d",
+    "url": "/on-track/static/js/2.00b58933.chunk.js"
   },
   {
     "revision": "a92de81bd61fb05311dce8d443e91858",
     "url": "/on-track/static/media/to-do-icon.a92de81b.png"
   },
   {
-    "revision": "77b758b9cbfcaba1f8a5374aab379a41",
+    "revision": "bbb2968ae66f1ffa4cae5b08242bca6c",
     "url": "/on-track/index.html"
   }
 ];
